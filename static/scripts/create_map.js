cb87919183ff7199d9e675076ca1850ef8a54e83
lukemitchell-n <lukemitchellnelson@gmail.com>
2025-08-03 20:12:31 -0700

    Site triggers tool and results returned to page

--- static/scripts/create_map.js
+++ static/scripts/create_map.js
@@ -1,23 +1,17 @@
-var map = L.map('map', { zoomControl:true, maxZoom:28, minZoom:10})
+var map = L.map('map', {
+    center: [-122.66, 45.519177],
+    zoom: 12,
+    zoomControl: true,
+    maxZoom: 28,
+    minZoom: 9
+})
 
 
 var hash = new L.Hash(map);
 map.attributionControl.setPrefix('<a href="https://github.com/tomchadwin/qgis2web" target="_blank">qgis2web</a> &middot; <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> &middot; <a href="https://qgis.org">QGIS</a>');
 var autolinker = new Autolinker({truncate: {length: 30, location: 'smart'}});
 
-function removeEmptyRowsFromPopupContent(content, feature) {
-    var tempDiv = document.createElement('div');
-    tempDiv.innerHTML = content;
-    var rows = tempDiv.querySelectorAll('tr');
-    for (var i = 0; i < rows.length; i++) {
-        var td = rows[i].querySelector('td.visible-with-data');
-        var key = td ? td.id : '';
-        if (td && td.classList.contains('visible-with-data') && feature.properties[key] == null) {
-            rows[i].parentNode.removeChild(rows[i]);
-             }
-    }
-    return tempDiv.innerHTML;
-}
+// Set up map controls
 L.control.locate({locateOptions: {maxZoom: 19}}).addTo(map);
 var measureControl = new L.Control.Measure({
     position: 'topleft',
@@ -37,208 +31,103 @@ function setBounds() {
     if (bounds_group.getLayers().length) {
         map.fitBounds(bounds_group.getBounds());
     }
-    map.setMaxBounds(map.getBounds());
+    map.setMaxBounds(map.getBounds().pad(.25));
 }
 
-//OSM background map layer
-map.createPane('pane_OpenStreetMap_0');
-map.getPane('pane_OpenStreetMap_0').style.zIndex = 400;
-var layer_OpenStreetMap_0 = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    pane: 'pane_OpenStreetMap_0',
+// --- OSM background map layer
+
+// - Work within a pane for the bg layer
+map.createPane('pane_OpenStreetMap');
+map.getPane('pane_OpenStreetMap').style.zIndex = 400;
+
+// - Set up the OSM layer and add it to the map
+var layer_OpenStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    pane: 'pane_OpenStreetMap',
     opacity: 1.0,
-    attribution: '',
-    minZoom: 10,
-    maxZoom: 28,
-    minNativeZoom: 0,
-    maxNativeZoom: 19
+    attribution: ''
 });
-layer_OpenStreetMap_0;
-map.addLayer(layer_OpenStreetMap_0);
-
-function pop_blocks_1(feature, layer) {
-    var popupContent = '<table>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['fid'] !== null ? autolinker.link(feature.properties['fid'].toLocaleString()) : '') + '</td>\
-            </tr>\
-        </table>';
-    layer.bindPopup(popupContent, {maxHeight: 400});
-    var popup = layer.getPopup();
-    var content = popup.getContent();
-    var updatedContent = removeEmptyRowsFromPopupContent(content, feature);
-    popup.setContent(updatedContent);
-}
+layer_OpenStreetMap;
+map.addLayer(layer_OpenStreetMap);
 
 
-//Isochrone - Accessible blocks layer
-function style_blocks_1_0() {
-    return {
-        pane: 'pane_blocks_1',
-        stroke: false,
-        fill: true,
-        fillOpacity: 1,
-        fillColor: 'rgba(255,0,4,.40)',
-        interactive: true,
+var baseMaps = { "OpenStreetMap": layer_OpenStreetMap };
+const layerControl = L.control.layers(baseMaps, {}).addTo(map);
+
+
+// Function to load a geojson layer from a file
+// Noted - leaflet seems to disagree with any geojson file
+// that uses a projected coordinate system.
+// All files should be projected to a GCS like WGS84
+async function loadGeoJsonData(file_path) {
+    try {
+        const response = await fetch(file_path);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error loading GeoJSON:', error);
+        return null;
     }
 }
 
-map.createPane('pane_blocks_1');
-map.getPane('pane_blocks_1').style.zIndex = 401;
-map.getPane('pane_blocks_1').style['mix-blend-mode'] = 'normal';
-var layer_blocks_1 = new L.geoJson(json_blocks_1, {
-    attribution: '',
-    interactive: true,
-    dataVar: 'json_blocks_1',
-    layerName: 'layer_blocks_1',
-    pane: 'pane_blocks_1',
-    //onEachFeature: pop_blocks_1,
-    style: style_blocks_1_0,
-});
-bounds_group.addLayer(layer_blocks_1);
-map.addLayer(layer_blocks_1);
+function addGeoJsonToMap(file_path, name, pane_name, z_index) {
+    var lyr
+    loadGeoJsonData(file_path).then(geojsonData => {
+        if (geojsonData) {
+            map.createPane(pane_name);
+            map.getPane(pane_name).style.zIndex = z_index;
 
+            lyr = L.geoJSON(geojsonData, {
+                pane: pane_name,
+                name: name
+            });
 
-//Isochrone - Accessible streets layer
-map.createPane('pane_street_network_3');
-map.getPane('pane_street_network_3').style.zIndex = 402;
-map.getPane('pane_street_network_3').style['mix-blend-mode'] = 'normal';
-var layer_street_network_3 = new L.geoJson(json_street_network_3, {
-    attribution: '',
-    interactive: true,
-    dataVar: 'json_street_network_3',
-    layerName: 'layer_street_network_3',
-    pane: 'pane_street_network_3',
-    style: style_street_network_3_0,
-});
-function style_street_network_3_0() {
-    return {
-        pane: 'pane_street_network_3',
-        opacity: 1,
-        color: 'rgba(100,200,100,1.0)',
-        dashArray: '',
-        lineCap: 'square',
-        lineJoin: 'bevel',
-        weight: .80,
-        fillOpacity: 0,
-        interactive: true,
+            lyr.addTo(map);
+            bounds_group.addLayer(lyr)
+            layerControl.addOverlay(lyr, name)
+        }
+    });
+}
+
+// If the tool has not yet been run, display the search bounds
+if (typeof filenames === 'undefined' || filenames == "Null") {
+    addGeoJsonToMap('/static/leaflet/data/search_area.geojson', "Search Area", "Search_pane", 401)
+}
+else {
+    console.log("filenames: " + filenames)
+    console.log("filenames[Area]: " + filenames["Area"])
+    if (filenames["Area"] && filenames["Area"] != "None") {
+        addGeoJsonToMap(filenames["Area"], "Area", "Area_Pane", 402);
+        console.log("Added area polygon to map at path " + filenames["Area"]);
+    }
+    if (filenames["Walking"] && filenames["Walking"] != "None") {
+        addGeoJsonToMap(filenames["Walking"], "Walking", "Walking_Pane", 402);
+        console.log("Added Walking polyline to map at path " + filenames["Walking"]);
+    }
+    if (filenames["Transit"] && filenames["Transit"] != "None") {
+        addGeoJsonToMap(filenames["Transit"], "Transit", "Transit_Pane", 402);
+        console.log("Added Transit polyline to map at path " + filenames["Transit"]);
     }
 }
 
-bounds_group.addLayer(layer_street_network_3);
-map.addLayer(layer_street_network_3);
-
-
-//Isochrone - Accessible transit layer
-map.createPane('pane_transit_network_2');
-map.getPane('pane_transit_network_2').style.zIndex = 403;
-map.getPane('pane_transit_network_2').style['mix-blend-mode'] = 'normal';
-var layer_transit_network_2 = new L.geoJson(json_transit_network_2, {
-    attribution: '',
-    interactive: true,
-    dataVar: 'json_transit_network_2',
-    layerName: 'layer_transit_network_2',
-    pane: 'pane_transit_network_2',
-    onEachFeature: pop_transit_network_2,
-    style: style_transit_network_2_0,
-});
-
-function pop_transit_network_2(feature, layer) {
-    var popupContent = '<table>\
-            <tr>\
-                <th scope="row">fid</th>\
-                <td>' + (feature.properties['fid'] !== null ? autolinker.link(feature.properties['fid'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['rte'] !== null ? autolinker.link(feature.properties['rte'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['dir'] !== null ? autolinker.link(feature.properties['dir'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['rte_desc'] !== null ? autolinker.link(feature.properties['rte_desc'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['dir_desc'] !== null ? autolinker.link(feature.properties['dir_desc'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['type'] !== null ? autolinker.link(feature.properties['type'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['stop_seq'] !== null ? autolinker.link(feature.properties['stop_seq'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['stop_id'] !== null ? autolinker.link(feature.properties['stop_id'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['stop_name'] !== null ? autolinker.link(feature.properties['stop_name'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['jurisdic'] !== null ? autolinker.link(feature.properties['jurisdic'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['zipcode'] !== null ? autolinker.link(feature.properties['zipcode'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['frequent'] !== null ? autolinker.link(feature.properties['frequent'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['start'] !== null ? autolinker.link(feature.properties['start'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['end'] !== null ? autolinker.link(feature.properties['end'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['cost'] !== null ? autolinker.link(feature.properties['cost'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['layer'] !== null ? autolinker.link(feature.properties['layer'].toLocaleString()) : '') + '</td>\
-            </tr>\
-            <tr>\
-                <td colspan="2">' + (feature.properties['path'] !== null ? autolinker.link(feature.properties['path'].toLocaleString()) : '') + '</td>\
-            </tr>\
-        </table>';
-    layer.bindPopup(popupContent, {maxHeight: 400});
-    var popup = layer.getPopup();
-    var content = popup.getContent();
-    var updatedContent = removeEmptyRowsFromPopupContent(content, feature);
-    popup.setContent(updatedContent);
-}
-
-function style_transit_network_2_0() {
-    return {
-        pane: 'pane_transit_network_2',
-        opacity: 1,
-        color: 'rgba(200,200,200,1.0)',
-        dashArray: '',
-        lineCap: 'square',
-        lineJoin: 'bevel',
-        weight: 1.0,
-        fillOpacity: 0,
-        interactive: true,
-    }
-}
-
-bounds_group.addLayer(layer_transit_network_2);
-map.addLayer(layer_transit_network_2);
-
-
-
-var baseMaps = {};
-L.control.layers(baseMaps,{'<img src="../legend/street_network_3.png" /> street_network': layer_street_network_3,'<img src="legend/transit_network_2.png" /> transit_network': layer_transit_network_2,'<img src="legend/blocks_1.png" /> blocks': layer_blocks_1,"OpenStreetMap": layer_OpenStreetMap_0,}).addTo(map);
-setBounds();
+//setBounds();
 
 var featureGroup = L.featureGroup().addTo(map);
 
 function onMapClick(e) {
     var coords = e.latlng;
+    var projcoords = map.options.crs.project(coords);
     var marker = new L.marker(e.latlng).addTo(map);
     var popupContent =
         '<form method="POST" role="form" id="form" enctype="multipart/form-data" class="form" >' +
             '<div class="form-group" style:"float:left;">' +
                 '<h4>Generate transit isochrone from this location</h4>' +
                 '<label for="lat">Lat: </label>' +
-                '<input id="lat" name="lat" type="text" readonly class="form-control" value="' + coords.lat.toFixed(6) + '"><br>' +
+                '<input id="lat" name="lat" type="text" readonly class="form-control" value="' + projcoords.x.toFixed(6) + '"><br>' +
                 '<label for="lon">Lon: </label>' +
-                '<input id="lon" name="lon" type="text" readonly class="form-control" value="' + coords.lng.toFixed(6) + '"><br>' +
+                '<input id="lon" name="lon" type="text" readonly class="form-control" value="' + projcoords.y.toFixed(6) + '"><br>' +
                 '<input id="crs" name="crs" type="hidden" value="' + map.options.crs.code + '">' +
                 '<label for="time">Time limit (minutes): </label>' +
                 '<input id="time" name="time" type="number" min="1" max ="60" class="form-control"><br>' +
