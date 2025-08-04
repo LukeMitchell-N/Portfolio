1e90d897a00ffba058b86f78d1d443617c0ec059
lukemitchell-n <lukemitchellnelson@gmail.com>
2025-08-04 13:40:30 -0700

    Update walking velocity

--- static/scripts/create_map.js
+++ static/scripts/create_map.js
@@ -1,11 +1,10 @@
 var map = L.map('map', {
-    center: [-122.66, 45.519177],
-    zoom: 12,
-    zoomControl: true,
+    center: [45.519177, -122.677],
+    zoom: 10,
+    zoomControl: false,
     maxZoom: 28,
     minZoom: 9
 })
-
 
 var hash = new L.Hash(map);
 map.attributionControl.setPrefix('<a href="https://github.com/tomchadwin/qgis2web" target="_blank">qgis2web</a> &middot; <a href="https://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> &middot; <a href="https://qgis.org">QGIS</a>');
@@ -25,14 +24,19 @@ measureControl.addTo(map);
 document.getElementsByClassName('leaflet-control-measure-toggle')[0].innerHTML = '';
 document.getElementsByClassName('leaflet-control-measure-toggle')[0].className += ' fas fa-ruler';
 
-var bounds_group = new L.featureGroup([]);
+var bounds_group = new L.featureGroup();
 
 function setBounds() {
+    console.log(bounds_group.getLayers())
     if (bounds_group.getLayers().length) {
         map.fitBounds(bounds_group.getBounds());
+        map.setMaxBounds(bounds_group.getBounds().pad(.25));
     }
-    map.setMaxBounds(map.getBounds().pad(.25));
 }
+var baseMaps = {};
+const layerControl = L.control.layers(baseMaps, {}).addTo(map);
+
+
 
 // --- OSM background map layer
 
@@ -41,17 +45,16 @@ map.createPane('pane_OpenStreetMap');
 map.getPane('pane_OpenStreetMap').style.zIndex = 400;
 
 // - Set up the OSM layer and add it to the map
-var layer_OpenStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    pane: 'pane_OpenStreetMap',
+var osm_layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 28,
     opacity: 1.0,
     attribution: ''
 });
-layer_OpenStreetMap;
-map.addLayer(layer_OpenStreetMap);
+map.addLayer(osm_layer);
+layerControl.addOverlay(osm_layer, "OpenStreetMaps")
 
 
-var baseMaps = { "OpenStreetMap": layer_OpenStreetMap };
-const layerControl = L.control.layers(baseMaps, {}).addTo(map);
+
 
 
 // Function to load a geojson layer from a file
@@ -73,48 +76,54 @@ async function loadGeoJsonData(file_path) {
 }
 
 function addGeoJsonToMap(file_path, name, pane_name, z_index) {
-    var lyr
-    loadGeoJsonData(file_path).then(geojsonData => {
+    return loadGeoJsonData(file_path).then(geojsonData => {
         if (geojsonData) {
             map.createPane(pane_name);
             map.getPane(pane_name).style.zIndex = z_index;
 
-            lyr = L.geoJSON(geojsonData, {
+            const lyr = L.geoJSON(geojsonData, {
                 pane: pane_name,
                 name: name
             });
 
             lyr.addTo(map);
-            bounds_group.addLayer(lyr)
+            lyr.addTo(bounds_group)
             layerControl.addOverlay(lyr, name)
         }
     });
 }
 
-// If the tool has not yet been run, display the search bounds
-if (typeof filenames === 'undefined' || filenames == "Null") {
-    addGeoJsonToMap('/static/leaflet/data/search_area.geojson', "Search Area", "Search_pane", 401)
-}
-else {
-    console.log("filenames: " + filenames)
-    console.log("filenames[Area]: " + filenames["Area"])
-    if (filenames["Area"] && filenames["Area"] != "None") {
-        addGeoJsonToMap(filenames["Area"], "Area", "Area_Pane", 402);
-        console.log("Added area polygon to map at path " + filenames["Area"]);
-    }
-    if (filenames["Walking"] && filenames["Walking"] != "None") {
-        addGeoJsonToMap(filenames["Walking"], "Walking", "Walking_Pane", 402);
-        console.log("Added Walking polyline to map at path " + filenames["Walking"]);
-    }
-    if (filenames["Transit"] && filenames["Transit"] != "None") {
-        addGeoJsonToMap(filenames["Transit"], "Transit", "Transit_Pane", 402);
-        console.log("Added Transit polyline to map at path " + filenames["Transit"]);
-    }
-}
 
-//setBounds();
+async function add_layers() {
+    if (typeof filenames === 'undefined' || filenames == "Null") {      // If the tool has not yet been run, display the search bounds
+        await addGeoJsonToMap(
+            '/static/leaflet/data/search_area.geojson',
+            "Search Area",
+            "Search_pane",
+            401
+        )
+    }
+    else {                                                              // otherwise, load the layers passed in through 'filenames'
+        console.log("filenames: " + filenames)
+        console.log("filenames[Area]: " + filenames["Area"])
+        if (filenames["Area"] && filenames["Area"] != "None") {
+            await addGeoJsonToMap(filenames["Area"], "Area", "Area_Pane", 402);
+            console.log("Added area polygon to map at path " + filenames["Area"]);
+        }
+        if (filenames["Walking"] && filenames["Walking"] != "None") {
+            await addGeoJsonToMap(filenames["Walking"], "Walking", "Walking_Pane", 402);
+            console.log("Added Walking polyline to map at path " + filenames["Walking"]);
+        }
+        if (filenames["Transit"] && filenames["Transit"] != "None") {
+            await addGeoJsonToMap(filenames["Transit"], "Transit", "Transit_Pane", 402);
+            console.log("Added Transit polyline to map at path " + filenames["Transit"]);
+        }
+    }
+    setBounds();
+}
+add_layers()
 
-var featureGroup = L.featureGroup().addTo(map);
+
 
 function onMapClick(e) {
     var coords = e.latlng;
@@ -132,7 +141,7 @@ function onMapClick(e) {
                 '<label for="time">Time limit (minutes): </label>' +
                 '<input id="time" name="time" type="number" min="1" max ="60" class="form-control"><br>' +
                 '<label for="velocity">Walking speed (kph): </label>' +
-                '<input id="velocity" name="velocity" type="number" min="0" max ="60" step="0.01" value="4.5" class="form-control"><br>' +
+                '<input id="velocity" name="velocity" type="number" min="0" max ="20" step="0.01" value="5.2" class="form-control"><br>' +
                 '<div class="form-group">' +
                     '<div style="text-align:center;" class="btn btn-primary"><button type="submit" value="submit" class="btn btn-primary trigger-submit">Submit</button></div>' +
                 '</div>' +
@@ -155,3 +164,4 @@ map.on('popupopen', function(e){
 map.on('popupclose', function(e){
     map.setMaxBounds(bounds_group.getBounds())});
 map.on('click', onMapClick);
+
